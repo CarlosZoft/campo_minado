9ad0815b5ca7fbe9dece05f5824c8f1255915f97
CarlosZoft <62192072+CarlosZoft@users.noreply.github.com>
2021-08-29 18:46:52 -0300

    feat: create Styles for the field component

---
+++ src/components/Field/Styles.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import params from "../../params";
+
+const styles = StyleSheet.create({
+  field: {
+    height: params.blockSize,
+    width: params.blockSize,
+    borderWidth: params.borderSize,
+  },
+  regular: {
+    backgroundColor: "#999",
+    borderLeftColor: "#CCC",
+    borderRightColor: "#CCC",
+    borderTopColor: "#333",
+    borderBottomColor: "#333",
+  },
+  opened: {
+    backgroundColor: "#999",
+    borderColor: "#777",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  label: {
+    fontWeight: "bold",
+    fontSize: params.fontSize,
+  },
+  exploded: {
+    backgroundColor: "red",
+    borderColor: "red",
+  },
+});
+
+export default styles;
