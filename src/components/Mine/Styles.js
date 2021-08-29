fe6f457d2e0b089017d45f4317edf886103bdc0c
CarlosZoft <62192072+CarlosZoft@users.noreply.github.com>
2021-08-29 18:47:26 -0300

    feat: create styles for ther mine component

---
+++ src/components/Mine/Styles.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  coreMine: {
+    height: 14,
+    width: 14,
+    borderRadius: 10,
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default styles;
