06ed477a6f17c66154e1ec40c654993a183326b9
CarlosZoft <62192072+CarlosZoft@users.noreply.github.com>
2021-09-01 00:24:09 -0300

    feat: add flag styles

---
+++ src/components/Flag/Styles.js
@@ -0,0 +1,39 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 2,
+  },
+  flagpole: {
+    position: "absolute",
+    height: 14,
+    width: 2,
+    backgroundColor: "#222",
+    marginLeft: 9,
+  },
+  flag: {
+    position: "absolute",
+    height: 5,
+    width: 6,
+    backgroundColor: "#F22",
+    marginLeft: 3,
+  },
+  base1: {
+    position: "absolute",
+    height: 2,
+    width: 6,
+    backgroundColor: "#222",
+    marginLeft: 7,
+    marginTop: 10,
+  },
+  base2: {
+    position: "absolute",
+    height: 2,
+    width: 10,
+    backgroundColor: "#222",
+    marginLeft: 5,
+    marginTop: 12,
+  },
+});
+
+export default styles;
